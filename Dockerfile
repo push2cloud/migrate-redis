379462a309b1966d3888154eb86bfa0f2c5c352d
Michael Erne <michael.erne@gmail.com>
2016-09-20 15:31:56 +0200

    adds initial version

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM mhart/alpine-node:6.6.0
+
+RUN apk add --no-cache jq bash redis && \
+    rm -f /etc/redis.conf \
+          /etc/logrotate.d/redis \
+          /etc/init.d/redis \
+          /etc/conf.d/redis \
+          /usr/bin/redis-benchmark \
+          /usr/bin/redis-sentinel \
+          /usr/bin/redis-server \
+          /usr/bin/redis-check-aof \
+          /usr/bin/redis-check-rdb \
+          /usr/share/licenses/redis/COPYING && \
+    npm install -g redis-dump
+
+COPY ./migrate.sh /migrate.sh
+
+CMD [ "/migrate.sh" ]
---
+++ migrate.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [[ -n ${DEBUG} ]] ; then
+  echo "DEBUG: VCAP_SERVICES:   ${VCAP_SERVICES}"
+  echo "DEBUG: fromService:     ${fromService}"
+  echo "DEBUG: toService:       ${toService}"
+fi
+
+
+if [[ -z ${VCAP_SERVICES} ]] ; then
+  echo "\${VCAP_SERVICES} not set!"
+  exit 1
+fi
+if [[ -z ${fromService} ]] ; then
+  echo "\${fromService} not set!"
+  exit 1
+fi
+if [[ -z ${toService} ]] ; then
+  echo "\${toService} not set!"
+  exit 1
+fi
+
+declare -A  from
+declare -A  to
+
+from[host]=$( echo "${VCAP_SERVICES}" | jq ".redis[] | select( .name == \"${fromService}\" ) | .credentials.host" | tr -d '"')
+from[password]=$( echo "${VCAP_SERVICES}" | jq ".redis[] | select( .name == \"${fromService}\" ) | .credentials.password" | tr -d '"')
+from[port]=$( echo "${VCAP_SERVICES}" | jq ".redis[] | select( .name == \"${fromService}\" ) | .credentials.port" | tr -d '"')
+
+to[host]=$( echo "${VCAP_SERVICES}" | jq ".redis[] | select( .name == \"${toService}\" ) | .credentials.host" | tr -d '"')
+to[password]=$( echo "${VCAP_SERVICES}" | jq ".redis[] | select( .name == \"${toService}\" ) | .credentials.password" | tr -d '"')
+to[port]=$( echo "${VCAP_SERVICES}" | jq ".redis[] | select( .name == \"${toService}\" ) | .credentials.port" | tr -d '"')
+
+if [[ -n ${DEBUG} ]] ; then
+  echo "DEBUG: from[host]:     ${from[host]}"
+  echo "DEBUG: from[password]: ${from[password]}"
+  echo "DEBUG: from[port]:     ${from[port]}"
+
+  echo "DEBUG: to[host]:     ${to[host]}"
+  echo "DEBUG: to[password]: ${to[password]}"
+  echo "DEBUG: to[port]:     ${to[port]}"
+fi
+
+
+if [[ -z ${from[host]} ]] ; then
+  echo "could not resolve details of \"${fromService}\" from \${VCAP_SERVICES}!"
+  exit 1
+fi
+if [[ -z ${to[host]} ]] ; then
+  echo "could not resolve details of \"${toService}\" from \${VCAP_SERVICES}!"
+  exit 1
+fi
+
+echo "Starting redis Migration"
+echo "  from: ${fromService}"
+echo "  to:   ${toService}"
+echo ""
+
+if [[ -n ${DEBUG} ]] ; then
+echo "Going to execute the following command:
+redis-dump \
+  -h ${from[host]} \
+  -p ${from[port]} \
+  -a ${from[password]} | \
+redis-cli \
+  -h ${to[host]} \
+  -p ${to[port]} \
+  -a ${to[password]} \
+"
+fi
+
+redis-dump \
+  -h ${from[host]} \
+  -p ${from[port]} \
+  -a ${from[password]} | \
+redis-cli \
+  -h ${to[host]} \
+  -p ${to[port]} \
+  -a ${to[password]}
+
+echo ""
+echo "Finished redis Migration with RC: $?"
+
+while true ; do
+  echo "This App can now be deleted"
+  sleep 60
+done
